49936ee0c1b67cdb388916993a89003c6c3f06ef
Pedro Muniz <joao.munnyz@gmail.com>
2026-07-17 12:19:37 -0300

    att

--- script/script.js
+++ script/script.js
@@ -7,38 +7,44 @@ let next = document.getElementById('next')
 let prev = document.getElementById('prev')
 let progress = document.getElementById('current-progress')
 let progressContainer = document.getElementById('progress-container')
+let playerN = document.getElementById('playerName')
 let isPlaying = false ;
 
 let dbgt = {
-    songName : 'Dan dan Kokoro Hikareteku' , 
+    songName : 'Dan dan kokoro hikareteku' , 
     artist : 'Field of View - Sanks' , 
     file : 'Dragon-Ball-Dan-Dan-Kokoro-Hikareteku' ,
     image : '892ab776279414977933b566575cf62d.gif' , 
-    theme : 'dragon-ball'
+    theme : 'dragon-ball' , 
+    playerName : ''
 }
 let dmc = {
     songName : 'Devils Never Cry' ,
     artist : 'Devil May Cry 3 - CAPCOM' ,
     file : 'Devils Never Cry' , 
-    image : 'artworks-w9hCYe774yygLUQt-zI0bPg-t1080x1080.png'
+    image : 'artworks-w9hCYe774yygLUQt-zI0bPg-t1080x1080.png' , 
+    playerName : 'Player : Pedro'
 }
 let katyperry = {
     songName : 'The One That Got Away',
     artist : 'Katy Perry',
     file: 'Katy Perry - The One That Got Away',
-    image : 'Screenshot_1.png'
+    image : 'Screenshot_1.png' , 
+    playerName : 'Player : Laura'
 };
 let sonic = {
     songName : 'Find your flame',
     artist : 'Sonic - Frontiers',
     file: 'Find Your Flame',
-    image : 'artworks-NHp0yctCFpypTkEI-5kzxMw-t500x500.png'
+    image : 'artworks-NHp0yctCFpypTkEI-5kzxMw-t500x500.png',
+    playerName : 'Player : Bruno'
 };
 let mj = {
     songName : 'Smooth Criminal',
     artist : 'Michael Jackson',
     file: 'MICHAEL JACKSON - SMOOTH CRIMINAL',
-    image : '28f5bc624ebe2476a3fbb4ffddd9e45e6526115228fc9.png'
+    image : '28f5bc624ebe2476a3fbb4ffddd9e45e6526115228fc9.png' ,
+    playerName : 'Player : Fernanda'
 };
 let playlist = [katyperry, sonic, mj , dmc , dbgt];
 let index = 0;
@@ -67,10 +73,16 @@ function initializeSong(){
     songName.textContent = `${playlist[index].songName}`;
     bandName.textContent = `${playlist[index].artist}`;
     audio.src = `songs/${playlist[index].file}.mp3`;
+    playerN.textContent = `${playlist[index].playerName}`
     
     document.body.classList.remove('dragon-ball')
+    cover.style.width ='400px'
+    cover.style.transition = '6s';
     if (playlist[index].theme === 'dragon-ball'){
         document.body.classList.add('dragon-ball');
+        cover.style.width = '700px' ;
+        cover.style.transition = '18s';
+        document.getElementById('title').textContent = 'Save Point';
     }
 }
 
