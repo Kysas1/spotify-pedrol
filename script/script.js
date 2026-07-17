87e6ce826b9d199a80edb7049114a740182e0eb2
Pedro Muniz <joao.munnyz@gmail.com>
2026-07-17 11:54:02 -0300

    atualização-spotify

--- script/script.js
+++ script/script.js
@@ -9,31 +9,38 @@ let progress = document.getElementById('current-progress')
 let progressContainer = document.getElementById('progress-container')
 let isPlaying = false ;
 
+let dbgt = {
+    songName : 'Dan dan Kokoro Hikareteku' , 
+    artist : 'Field of View - Sanks' , 
+    file : 'Dragon-Ball-Dan-Dan-Kokoro-Hikareteku' ,
+    image : '892ab776279414977933b566575cf62d.gif' , 
+    theme : 'dragon-ball'
+}
 let dmc = {
     songName : 'Devils Never Cry' ,
     artist : 'Devil May Cry 3 - CAPCOM' ,
     file : 'Devils Never Cry' , 
-    image : 'artworks-w9hCYe774yygLUQt-zI0bPg-t1080x1080'
+    image : 'artworks-w9hCYe774yygLUQt-zI0bPg-t1080x1080.png'
 }
 let katyperry = {
     songName : 'The One That Got Away',
     artist : 'Katy Perry',
     file: 'Katy Perry - The One That Got Away',
-    image : 'Screenshot_1'
+    image : 'Screenshot_1.png'
 };
 let sonic = {
     songName : 'Find your flame',
     artist : 'Sonic - Frontiers',
     file: 'Find Your Flame',
-    image : 'artworks-NHp0yctCFpypTkEI-5kzxMw-t500x500'
+    image : 'artworks-NHp0yctCFpypTkEI-5kzxMw-t500x500.png'
 };
 let mj = {
     songName : 'Smooth Criminal',
     artist : 'Michael Jackson',
     file: 'MICHAEL JACKSON - SMOOTH CRIMINAL',
-    image : '28f5bc624ebe2476a3fbb4ffddd9e45e6526115228fc9'
+    image : '28f5bc624ebe2476a3fbb4ffddd9e45e6526115228fc9.png'
 };
-let playlist = [katyperry, sonic, mj , dmc];
+let playlist = [katyperry, sonic, mj , dmc , dbgt];
 let index = 0;
 function playSong (){ 
     play.querySelector('.bi').classList.remove('bi-play-circle-fill');
@@ -56,10 +63,15 @@ function playpauseDecider() {
     }
 }
 function initializeSong(){
-    cover.src = `images/${playlist[index].image}.png`;
+    cover.src = `images/${playlist[index].image}`;
     songName.textContent = `${playlist[index].songName}`;
     bandName.textContent = `${playlist[index].artist}`;
     audio.src = `songs/${playlist[index].file}.mp3`;
+    
+    document.body.classList.remove('dragon-ball')
+    if (playlist[index].theme === 'dragon-ball'){
+        document.body.classList.add('dragon-ball');
+    }
 }
 
 function previ(){
